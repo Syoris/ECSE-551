70fe2516e7bcacdf615147554776ebfb45fc1f2f
Charles <charles.sirois14@gmail.com>
2023-11-02 16:10:05 -0400

    NB working

--- MP2/NB.md
+++ MP2/NB.md
@@ -9,7 +9,7 @@ $$ P(y=k|x) \propto \delta_k(x) = \log{P(y=k)P(x|y=k)} $$
 
 $$  = \log{P(y=k)\sum_{j=1}^m P(x_j | y=k)} $$
 
-$$  = \log{[\theta_k\sum_{j=1}^m \theta_{j, k}^{x_j} (1 - \theta_{j, k})^{1-x_j} ]}$$
+$$ \delta_k(x) = \log{[\theta_k\sum_{j=1}^m \theta_{j, k}^{x_j} (1 - \theta_{j, k})^{1-x_j} ]}$$
 
 where:
 $$ \theta_k = P(y=k) = \frac{\#\text{ samples where }y=k}{\#\text{ samples}} $$
@@ -19,8 +19,17 @@ $$ \theta_{j, k} = P(x_j=1 | y=k) = \frac{\#\text{ samples where }x_j=1 \text{ a
 Classify the output as:
 $$ \text{Output} = \argmax_k \delta_k(x) $$
 
+### Prediction
+$$ \delta_k(x) = \log{\theta_k} + \sum_{j=1}^m[ x_j * \log{\theta_{j, k}} +  (1-x_j)*\log{(1 - \theta_{j, k})^ ]}$$
+
 ## Laplace Smoothing
 To deal w/ unobserved words in training data.
 
 Replace $\theta_{j, k}$ with:
 $$ \theta_{j, k} = P(x_j=1 | y=k) = \frac{(\#\text{ samples where }x_j=1 \text{ and }y=k) + 1}{(\#\text{ samples where }y=k) + 2}  $$ 
+
+
+## SK Discriminants
+
+jll = safe_sparse_dot(X, (feat_log_proba - feat_log_neg_proba).T)
+        jll += log_class_prior + feat_log_neg_proba.sum(axis=1)
--- MP2/NaiveBayes.py
+++ MP2/NaiveBayes.py
@@ -26,7 +26,7 @@ class NaiveBayes:
     Attributes:
         thetas (ndarray): ndarray of shape (k, (1 + m))
           Theta values for classification
-        labels: Output labels
+        class: Output class
         n_features (int): Dimension of features (m). 0 if model not fitted.
         n_iter (int): Number of iterations needed for fitting. 0 if not fitted.
 
@@ -38,26 +38,32 @@ class NaiveBayes:
         self,
         laplace_smoothing: bool = True,
         k_cv: int = 0,  # k-fold cross-validation
+        verbose=False,  # To print execution info
     ) -> None:
+        self._n_features = 0
 
-        self.weights = None
-        self.n_features = 0
-        self.n_labels = 0
-        self.n_iter = 0
+        self._classes = None
+        self._n_class = 0
+        self._class_count = None
+        self._n_samples = 0
 
-        self.X_train = None
-        self.y_train = None
+        self.X = None  # X dataset for training
+        self.y = None  # y dataset for training
 
         self.laplace_smoothing = laplace_smoothing
 
         self.k_cv = k_cv
 
-        self._weight_array = []  # Array with progression of weights during training
+        self._log_class_prior = None
+        self._feat_log_proba = None
+        self._feat_log_proba = None
 
         self.results = None  # Results dataframe
         self._comp_time = None
 
-    def fit(self, X, y, w_start=None, verbose=False):
+        self._verbose = verbose
+
+    def fit(self, X, y):
         """
         Fit the model according to the given training data.
 
@@ -71,29 +77,30 @@ class NaiveBayes:
             w (ndarray, optional): shape (n_features, )
             T   o give an initial guess
 
-            verbose (bool):
-                To print info
-
         Returns:
             self
                 Model with weights fitted to training dataset
         """
-        self.X_train = X
-        self.y_train = y
+        self.X = X
+        self.y = y
 
-        n, self.n_features = X.shape
+        # Check dataset sizes
+        self._n_samples, self._n_features = X.shape
 
         if X.shape[0] != y.shape[0]:
             raise ValueError(
                 f"Mismatch between the size of the input ({X.shape[0]}) and outputs ({y.shape[0]})"
             )
-        
-        # Number of labels
-        self.n_labels = ...
 
-        if verbose:
+        # Number of class
+        self._classes, self._class_count = np.unique(self.y, return_counts=True)
+        self._n_class = len(self._classes)
+
+        if self._verbose:
             print(f"Fitting Naive Bayes model for the dataset")
-            print(f"\t# Features: {self.n_features}, # Labels: {self.n_labels}, # Samples: {n}")
+            print(
+                f"\t# Features: {self._n_features}, classes: {self._classes}, # Samples: {self._n_samples}"
+            )
             if self.k_cv != 0:
                 print(f"\tUsing {self.k_cv}-Fold Cross-Validation")
             else:
@@ -101,11 +108,11 @@ class NaiveBayes:
 
         # No cross-validation
         if self.k_cv == 0:
-            self.train_model()
+            self._train_model()
 
         # k-fold cross-validation
         else:
-            self.cross_validation(X, y, w_start, verbose)
+            self._cross_validation()
 
         return self
 
@@ -115,7 +122,7 @@ class NaiveBayes:
 
         For each class, computes:
             $$  \delta_k = \log{[\theta_k\sum_{j=1}^m \theta_{j, k}^{x_j} (1 - \theta_{j, k})^{1-x_j} ]}$$
-        
+
         Classify the output as:
             $$ \text{Output} = \argmax_k \delta_k(x) $$
 
@@ -128,19 +135,48 @@ class NaiveBayes:
         Returns:
             ndarray: Predicted output of each data point (nx1)
         """
-        if self.weights is None:
+        if self._thetas is None:
             raise ValueError(f"Model is not trained.")
 
-        y_pred = ...
+        log_class_prior = np.log(self._thetas[:, 0])  # log( P(y=k) )
+        feat_log_proba = np.log(self._thetas[:, 1::])  # log( P(x_j=1 | y=k) )
+        feat_log_neg_proba = np.log(
+            1 - self._thetas[:, 1::]
+        )  # log( 1 - P(x_j=1 | y=k) )
 
-        return y_pred
+        n_samples = X.shape[0]
+        predictions = []
+        self._joint_log_likelihood = np.zeros((n_samples, self._n_class))
+
+        # # SK
+        # from sklearn.utils.extmath import safe_sparse_dot
+
+        # jll = safe_sparse_dot(X, (feat_log_proba - feat_log_neg_proba).T)
+        # jll += log_class_prior + feat_log_neg_proba.sum(axis=1)
+
+        for sample_num in range(n_samples):
+            X_pred = X[sample_num, :]
+
+            feat_proba = np.sum(
+                feat_log_proba * X_pred + feat_log_neg_proba * (1 - X_pred), axis=1
+            )
+
+            discriminants = log_class_prior + feat_proba
+
+            self._joint_log_likelihood[sample_num, :] = discriminants
+
+            y_pred = self._classes[np.argmax(discriminants, axis=None)]
+
+            predictions.append(y_pred)
+
+        return predictions
 
     def accu_eval(self, X, y):
         """To compute the accuracy of the model
 
         Args:
             X (ndarray): Test samples
-            y (ndarray): True labels of X
+            y (ndarray): True class of X
 
         Returns:
             float: Accuracy of the model over test samples
@@ -152,7 +188,7 @@ class NaiveBayes:
         return accuracy
 
     # TODO
-    def cross_validation(self, X, y, w_start, verbose=False):
+    def _cross_validation(self):
         """Perform k-fold cross validation for a given model (set of parameters)/
 
         Args:
@@ -262,7 +298,7 @@ class NaiveBayes:
         converged = max_acc_row["Converged"]
 
         # Train model on whole dataset with best params
-        if verbose:
+        if self._verbose:
             print(f"Max accuracy obtained: {acc_max}")
             print(f"\tAlpha: {alpha_max}")
             print(f"\tTolerance: {tol_max}")
@@ -293,36 +329,35 @@ class NaiveBayes:
 
         return results  # acc, comp time, parameters
 
-    def train_model(self):
+    def _train_model(self):
         """Compute the theta needed to estimate the probabilities
 
         For each class:
             $$ \theta_k = P(y=k) = (# samples where y=k) / (# samples) $$
-            $$ \theta_{j, k} = P(x_j=1 | y=k) = (# samples where x_j=1 and y=k) / (# samples where y=k)  $$ 
+            $$ \theta_{j, k} = P(x_j=1 | y=k) = (# samples where x_j=1 and y=k) / (# samples where y=k)  $$
 
         Stores the values in a np.array of shape k x (1 + m)
-            -> \theta_{k} = thetas[k, 0], k=0, ... n_labels
+            -> \theta_{k} = thetas[k, 0], k=0, ... n_class  (prior of class k)
             -> \theta_{j, k} = thetas[k, j], j=1, ..., m
-
         """
-        self.thetas = np.zeros(self.n_labels, (self.n_features + 1))
+        self._thetas = np.zeros([self._n_class, self._n_features + 1])
 
-        for k in range(self.n_labels):
-            n_total = ... # n total of samples
+        for k, class_label in enumerate(self._classes):
+            n_yk = self._class_count[k]  # n samples where y=k
 
-            n_y_k = ... # n samples where y=k
+            X_k = self.X[self.y == class_label, :]
 
-            theta_k = ... # \theta_k
+            theta_k = n_yk / self._n_samples  # P(y=k), prior for class k
+            self._thetas[k, 0] = theta_k
 
-            self.thetas[k, 0] = theta_k
+            for j in range(self._n_features):
+                samples_j_k = X_k[:, j] == 1  # Array with True where X_j is 1
 
-            for j in range(1, self.n_features):
-                n_x_k = ... # n samples where y=k and x=x_j
+                n_xj_yk = samples_j_k.sum()  # n samples where y=k and x=x_j
 
-                theta_j_k = ... # \theta_{j, k} 
+                if self.laplace_smoothing:
+                    theta_j_k = (n_xj_yk + 1) / (n_yk + 2)
+                else:
+                    theta_j_k = n_xj_yk / n_yk
 
-                self.thetas[k, j] = theta_j_k
-
-
-            if self.laplace_smoothing:
-                ...
+                self._thetas[k, j + 1] = theta_j_k  # \theta_{j, k}
